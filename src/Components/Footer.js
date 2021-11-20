20d6f1d03d4fe8a885e227d8ea06cb0e88affcba
Ahmad <ahmadghojah1996@gmail.com>
2021-11-20 21:29:04 +0200

    fixed links

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,9 +5,9 @@ const Footer = () => {
         <footer style={{ backgroundColor: 'rgb(102, 102, 102)', textAlign: 'center', color: '#fff' }}>
             {window.innerWidth < 768 ?
                 <nav style={{justifyContent:'center',paddingTop:'16px'}}>
-                    <a style={{color:'#fff'}} className="navLink" href='/سياسة-الخصوصية' title="سياسة-الخصوصية">سياسة الخصوصية</a>
-                    <a style={{color:'#fff'}} className="navLink" href='/من-نحن' title="من-نحن">من نحن</a>
-                    <a style={{color:'#fff'}} className="navLink" href='/تواصل-معنا' title="تواصل-معنا">تواصل معنا</a>
+                    <a style={{color:'#fff'}} className="navLink" href='https://ghojah.com/%D8%B3%D9%8A%D8%A7%D8%B3%D8%A9-%D8%A7%D9%84%D8%AE%D8%B5%D9%88%D8%B5%D9%8A%D8%A9' title="سياسة-الخصوصية">سياسة الخصوصية</a>
+                    <a style={{color:'#fff'}} className="navLink" href='https://ghojah.com/%D9%85%D9%86-%D9%86%D8%AD%D9%86' title="من-نحن">من نحن</a>
+                    <a style={{color:'#fff'}} className="navLink" href='https://ghojah.com/%D8%AA%D9%88%D8%A7%D8%B5%D9%84-%D9%85%D8%B9%D9%86%D8%A7' title="تواصل-معنا">تواصل معنا</a>
                 </nav>  : ''}
                 {window.innerWidth < 768 ?<hr style={{margin:'16px 0 0'}}/>:''}
             © 2016-2050 All Rights Reserved
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -7,9 +7,9 @@ class Nav extends Component {
     render() {
         return (window.innerWidth>=768 ? 
         <nav>
-            <a className="navLink" href='/سياسة-الخصوصية' title="سياسة-الخصوصية">سياسة الخصوصية</a>
-            <a className="navLink" href='/من-نحن' title="من-نحن">من نحن</a>
-            <a className="navLink" href='/تواصل-معنا' title="تواصل-معنا">تواصل معنا</a>
+            <a className="navLink" href='https://ghojah.com/%D8%B3%D9%8A%D8%A7%D8%B3%D8%A9-%D8%A7%D9%84%D8%AE%D8%B5%D9%88%D8%B5%D9%8A%D8%A9' title="سياسة-الخصوصية">سياسة الخصوصية</a>
+            <a className="navLink" href='https://ghojah.com/%D9%85%D9%86-%D9%86%D8%AD%D9%86' title="من-نحن">من نحن</a>
+            <a className="navLink" href='https://ghojah.com/%D8%AA%D9%88%D8%A7%D8%B5%D9%84-%D9%85%D8%B9%D9%86%D8%A7' title="تواصل-معنا">تواصل معنا</a>
         </nav> : '');
     }
 }
